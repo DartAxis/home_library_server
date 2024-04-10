6bf995800985180d57289ead75807c84dd3c5c2b
dartaxis <filatyev.a.a@gmail.com>
2024-04-10 15:19:27 +0300

    прикрутил добавление истории просмотра книг

--- src/main/java/ru/dartinc/library_server/model/HistoryView.java
+++ src/main/java/ru/dartinc/library_server/model/HistoryView.java
@@ -1,11 +1,19 @@
 package ru.dartinc.library_server.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.dartinc.library_server.security.model.User;
 
 import java.time.LocalDateTime;
 
 @Entity(name = "view_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HistoryView {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/ru/dartinc/library_server/services/BookService.java
+++ src/main/java/ru/dartinc/library_server/services/BookService.java
@@ -3,10 +3,12 @@ package ru.dartinc.library_server.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.dartinc.library_server.dto.BookInDTO;
 import ru.dartinc.library_server.model.*;
 import ru.dartinc.library_server.repository.BookRepository;
+import ru.dartinc.library_server.security.model.User;
 import ru.dartinc.library_server.utils.Base64Utils;
 import ru.dartinc.library_server.utils.FileLibUtils;
 import ru.dartinc.library_server.utils.SevenZCompress;
@@ -33,6 +35,8 @@ public class BookService {
     private final PubHouseService pubHouseService;
     private final TagService tagService;
     private final SeriaService seriaService;
+
+    private final HistoryViewService historyViewService;
 
     @Value("${pathtostorage}")
     private String pathToStorage;
@@ -283,9 +287,15 @@ public class BookService {
     }
     public String getBookFile(Long id) {
         var result =getBookById(id);
+        User principal =(User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if(result!= null){
             try {
                 SevenZDecompress.decompress(pathToStorage + result.getPathToZipBook(), pathToTemp);
+                HistoryView historyElement = new HistoryView();
+                historyElement.setBook(result);
+                historyElement.setUser(principal);
+                historyElement.setViewDate(LocalDateTime.now());
+                historyViewService.save(historyElement);
                 return pathToTemp + result.getUuid()+"."+result.getFileFormatBook().toLowerCase();
             } catch (IOException e){
                 e.printStackTrace();
